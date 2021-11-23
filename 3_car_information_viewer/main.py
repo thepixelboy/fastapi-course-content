4955e344b946c7b8c84532fd1314c5e94173c418
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-23 18:05:56 +0100

    feat: create car feature

--- 3_car_information_viewer/main.py
+++ 3_car_information_viewer/main.py
@@ -68,13 +68,40 @@ def get_car_by_id(request: Request, id: int = Path(..., ge=0, lt=1000)):
     return response
 
 
+@app.get("/create", response_class=HTMLResponse)
+def create_car(request: Request):
+    return templates.TemplateResponse(
+        "create.html", {"request": request, "title": "Create Car"}
+    )
+
+
 @app.post("/search", response_class=RedirectResponse)
 def search_cars(id: str = Form(...)):
     return RedirectResponse("/cars/" + id, status_code=302)
 
 
 @app.post("/cars", status_code=status.HTTP_201_CREATED)
-def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
+def add_cars(
+    make: Optional[str] = Form(...),
+    model: Optional[str] = Form(...),
+    year: Optional[str] = Form(...),
+    price: Optional[float] = Form(...),
+    engine: Optional[str] = Form(...),
+    autonomous: Optional[bool] = Form(...),
+    sold: Optional[List[str]] = Form(None),
+    min_id: Optional[int] = Body(0),
+):
+    body_cars = [
+        Car(
+            make=make,
+            model=model,
+            year=year,
+            price=price,
+            engine=engine,
+            autonomous=autonomous,
+            sold=sold,
+        )
+    ]
     if len(body_cars) < 1:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="No cars to add."
@@ -86,6 +113,8 @@ def add_cars(body_cars: List[Car], min_id: Optional[int] = Body(0)):
             min_id += 1
         cars[min_id] = car
         min_id += 1
+
+    return RedirectResponse(url="/cars", status_code=302)
 
 
 @app.put("/cars/{id}", response_model=Dict[str, Car])
