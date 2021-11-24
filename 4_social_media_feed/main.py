c5cf255916bd0d2afb26cdb06eb0866fc55bec7e
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-24 09:34:35 +0100

    feat: create base home page

--- 4_social_media_feed/main.py
+++ 4_social_media_feed/main.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -32,6 +32,8 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-@app.get("/")
-def root():
-    return {"set": "up!"}
+@app.get("/", response_class=HTMLResponse)
+def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "FriendConnect - Home"}
+    )
