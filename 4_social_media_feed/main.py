a207b4d052b0b652452e816ae61d1283dfaf03c2
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-24 08:38:45 +0100

    feat: creating models

--- 4_social_media_feed/main.py
+++ 4_social_media_feed/main.py
@@ -1,7 +1,31 @@
+from typing import List, Optional
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+from db import users
+
+
+class Notification(BaseModel):
+    author: str
+    description: str
+
+
+class User(BaseModel):
+    name: str
+    username: str
+    email: str
+    birthday: str
+    friends: List[str]
+    notifications: List[Notification]
+
+
+class UserDB(User):
+    hashed_password: str
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
