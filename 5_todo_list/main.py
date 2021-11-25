b06dce346e6c641d88c80df57a67b840d6a40c94
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-25 11:39:45 +0100

    feat: create home page

--- 5_todo_list/main.py
+++ 5_todo_list/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -8,5 +8,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
-def root():
-    return {"set": "up!"}
+def root(request: Request):
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "Home"}
+    )
