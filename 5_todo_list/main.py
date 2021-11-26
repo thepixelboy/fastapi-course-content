b24730be27de36cd8488536f9894cea34d5de6c5
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-26 14:36:48 +0100

    feat: logout mechanism

--- 5_todo_list/main.py
+++ 5_todo_list/main.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 
 from dotenv import load_dotenv
-from fastapi import Depends, FastAPI, Form, Request, status
+from fastapi import Depends, FastAPI, Form, Request, Response, status
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import RedirectResponse
 from fastapi.security import OAuth2PasswordRequestForm
@@ -172,3 +172,11 @@ def register(
             {"request": request, "title": "Register", "invalid": True},
             status_code=status.HTTP_400_BAD_REQUEST,
         )
+
+
+@app.get("/logout")
+def logout(response: Response):
+    response = RedirectResponse("/")
+    manager.set_cookie(response, None)
+
+    return response
