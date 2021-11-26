b460a177ba956ff9a43446996ae9d5848f21f1f6
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-11-26 11:05:22 +0100

    feat: login page and mechanism. setup and crud

---
+++ 5_todo_list/crud.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Session
+
+import models
+import schemas
+
+
+def get_user(db: Session, id: str):
+    return db.query(models.User).filter(models.User.id == id).first()
+
+
+def get_user_by_username(db: Session, username: str):
+    return (
+        db.query(models.User).filter(models.User.username == username).first()
+    )
--- 5_todo_list/main.py
+++ 5_todo_list/main.py
@@ -1,11 +1,23 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, Request
 from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi_login.fastapi_login import LoginManager
 from sqlalchemy.orm import Session
 
 import models
 from db import DBContext, SessionLocal, engine
+
+load_dotenv()
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ACCESS_TOKEN_EXPIRES_MINUTES = 60
+
+manager = LoginManager(SECRET_KEY, token_url="/login", use_cookie=True)
+manager.cookie_name = "auth"
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -27,3 +39,10 @@ def root(request: Request):
 @app.get("/tasks")
 def get_tasks(db: Session = Depends(get_db)):
     return jsonable_encoder(db.query(models.Task).first())
+
+
+@app.get("/login")
+def get_login(request: Request):
+    return templates.TemplateResponse(
+        "login.html", {"request": request, "title": "Login"}
+    )
